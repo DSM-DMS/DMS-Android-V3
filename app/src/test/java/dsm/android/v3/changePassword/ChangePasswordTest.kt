9839d666c2bcd3bea6204ae4f29200d3c61ae166
이종현 <dwg7691@gmail.com>
2018-12-27 11:23:18 +0900

    [UPDATE] Change Test naming / logic

--- app/src/test/java/dsm/android/v3/changePassword/ChangePasswordTest.kt
+++ app/src/test/java/dsm/android/v3/changePassword/ChangePasswordTest.kt
@@ -2,6 +2,7 @@ package dsm.android.v3.changePassword
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import dsm.android.v3.ui.changePassword.ChangePasswordViewModel
+import dsm.android.v3.util.testObserver
 import org.junit.Before
 import org.junit.Test
 import org.junit.Assert.*
@@ -24,27 +25,35 @@ class ChangePasswordTest {
 
     @Test
     fun `1) 새로운 패스워드랑 패스워드 확인이랑 다를 때`() {
+        val inputStatus = viewModel.inputStatus.testObserver()
+
         viewModel.originPassword.value = "asdfd"
         viewModel.newPassword.value = "asdf"
         viewModel.confirmPassword.value = "asdfdd"
 
-        assertFalse(viewModel.statusMerger.value!!)
+        assertFalse(inputStatus.observedValues.last()!!)
     }
 
     @Test
-    fun whenOriginPasswordNotInput() {
+    fun `2) 기존 비밀번호 입력 안 했을때`() {
+        val inputStatus = viewModel.inputStatus.testObserver()
+
         viewModel.newPassword.value = "asdf"
         viewModel.confirmPassword.value = "asdf"
 
-        assertFalse(viewModel.statusMerger.value!!)
+        assertFalse(inputStatus.observedValues.last()!!)
     }
 
     @Test
-    fun whenCorrectInput() {
+    fun `3) 입력을 올바르게 잘 했을 때`() {
+        val inputStatus = viewModel.inputStatus.testObserver()
+
         viewModel.originPassword.value = "asdfd"
         viewModel.newPassword.value = "asdf"
         viewModel.confirmPassword.value = "asdf"
 
-        assertTrue(viewModel.statusMerger.value!!)
+        assertTrue(inputStatus.observedValues.last()!!)
     }
+
+
 }
