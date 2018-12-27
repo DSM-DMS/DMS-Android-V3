78ac4f50b3992601bb9e830331fb95eb9c26ba23
이종현 <dwg7691@gmail.com>
2018-12-27 09:09:12 +0900

    [ADD] ChangePasswordTest

--- app/src/test/java/dsm/android/v3/changePassword/ChangePasswordTest.kt
+++ app/src/test/java/dsm/android/v3/changePassword/ChangePasswordTest.kt
@@ -1,7 +1,50 @@
 package dsm.android.v3.changePassword
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import dsm.android.v3.ui.changePassword.ChangePasswordViewModel
 import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Rule
+import org.mockito.MockitoAnnotations
+
 
 class ChangePasswordTest {
+    lateinit var viewModel: ChangePasswordViewModel
 
+    @Rule
+    @JvmField
+    public val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this);
+        viewModel = ChangePasswordViewModel()
+    }
+
+    @Test
+    fun `1) 새로운 패스워드랑 패스워드 확인이랑 다를 때`() {
+        viewModel.originPassword.value = "asdfd"
+        viewModel.newPassword.value = "asdf"
+        viewModel.confirmPassword.value = "asdfdd"
+
+        assertFalse(viewModel.statusMerger.value!!)
+    }
+
+    @Test
+    fun whenOriginPasswordNotInput() {
+        viewModel.newPassword.value = "asdf"
+        viewModel.confirmPassword.value = "asdf"
+
+        assertFalse(viewModel.statusMerger.value!!)
+    }
+
+    @Test
+    fun whenCorrectInput() {
+        viewModel.originPassword.value = "asdfd"
+        viewModel.newPassword.value = "asdf"
+        viewModel.confirmPassword.value = "asdf"
+
+        assertTrue(viewModel.statusMerger.value!!)
+    }
 }
