84fdcd6128f8eb25e16d0ec921060a038bb8e98c
LeeSungHyun <joooosan011@gmail.com>
2019-03-12 15:20:00 +0900

    [UPDATE] notice_notice, notice_rules, notice_question, notice_main

---
+++ app/src/main/java/dsm/android/v3/ui/notice/NoticeFragment.kt
@@ -0,0 +1,20 @@
+package dsm.android.v3.ui.notice
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NoticeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+}
