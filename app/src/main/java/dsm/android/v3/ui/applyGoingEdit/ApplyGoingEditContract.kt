d03a97462b5d4844b1a12299aeae032f733c762f
jinusong <sugwer7464@gmail.com>
2019-03-23 16:48:19 +0900

    [Update] ApplyEditRegex

--- app/src/main/java/dsm/android/v3/ui/applyGoingEdit/ApplyGoingEditContract.kt
+++ app/src/main/java/dsm/android/v3/ui/applyGoingEdit/ApplyGoingEditContract.kt
@@ -2,8 +2,5 @@ package dsm.android.v3.ui.applyGoingEdit
 
 interface ApplyGoingEditContract {
     fun createShortToast(text: String)
-    fun setErrorApplyGoingGoDate(text: String)
-    fun setErrorApplyGoingGoTime(text: String)
-    fun setErrorApplyGoingReason(text: String)
     fun backApplyGoing()
 }
