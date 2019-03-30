b5c22895d51203232250b094a49b14f34bb2ed14
PaperEd <dwg7691@gmail.com>
2019-03-30 23:18:07 +0900

    Create ApplyMusicAdapter.kt

---
+++ app/src/main/java/dsm/android/v3/adapter/ApplyMusicAdapter.kt
@@ -0,0 +1,34 @@
+package dsm.android.v3.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dsm.android.v3.model.ApplyMusicDetailModel
+import dsm.android.v3.databinding.ItemApplymusicLogBinding
+
+class ApplyMusicAdapter : RecyclerView.Adapter<ApplyMusicAdapter.ApplyMusicViewHolder>() {
+    var item = arrayListOf<ApplyMusicDetailModel>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(view: ViewGroup, p1: Int): ApplyMusicViewHolder {
+        val binding = ItemApplymusicLogBinding.inflate(LayoutInflater.from(view.context), view, false)
+        return ApplyMusicViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = item.size
+
+    override fun onBindViewHolder(viewHolder: ApplyMusicViewHolder, p1: Int) {
+        viewHolder.bind()
+    }
+
+    inner class ApplyMusicViewHolder(val binding: ItemApplymusicLogBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
+            binding.musicData = item[adapterPosition]
+        }
+
+    }
+}
