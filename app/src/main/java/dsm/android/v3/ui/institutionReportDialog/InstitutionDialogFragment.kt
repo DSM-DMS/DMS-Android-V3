03362063fbe9debf5e06b879fce9aee4c5741d82
jinusong <sugwer7464@gmail.com>
2019-04-12 23:43:26 +0900

    [Refactory] InstitutionDialog

--- app/src/main/java/dsm/android/v3/ui/institutionReportDialog/InstitutionDialogFragment.kt
+++ app/src/main/java/dsm/android/v3/ui/institutionReportDialog/InstitutionDialogFragment.kt
@@ -1,5 +1,6 @@
 package dsm.android.v3.ui.institutionReportDialog
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -13,15 +14,17 @@ import dsm.android.v3.util.DataBindingDialogFragment
 import kotlinx.android.synthetic.main.dialog_institution_report.*
 import org.jetbrains.anko.support.v4.toast
 
-class InstitutionDialogFragment: DataBindingDialogFragment<DialogInstitutionReportBinding>(), InstitutionReportContract {
+class InstitutionDialogFragment: DataBindingDialogFragment<DialogInstitutionReportBinding>() {
 
     override val layoutId: Int
         get() = R.layout.dialog_institution_report
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val factory = InstitutionReportViewModelFactory(this)
-        binding.institutionReportViewModel = ViewModelProviders.of(this, factory).get(InstitutionReportViewModel::class.java)
+        binding.institutionReportViewModel = ViewModelProviders.of(this).get(InstitutionReportViewModel::class.java)
+
+        binding.institutionReportViewModel!!.toastLiveData.observe(this, Observer { toast(it!!) })
+        binding.institutionReportViewModel!!.exitInstitutionReportEvent.observe(this, Observer { dialog.dismiss() })
         return rootView
     }
 
@@ -29,8 +32,4 @@ class InstitutionDialogFragment: DataBindingDialogFragment<DialogInstitutionRepo
         super.onStart()
         dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
-
-    override fun createShortToast(text: String) = toast(text).show()
-
-    override fun exitInstitutionReport() = dialog.dismiss()
 }
--- app/src/main/java/dsm/android/v3/ui/institutionReportDialog/InstitutionReportContract.kt
+++
@@ -1,6 +0,0 @@
-package dsm.android.v3.ui.institutionReportDialog
-
-interface InstitutionReportContract{
-    fun exitInstitutionReport()
-    fun createShortToast(text: String)
-}
