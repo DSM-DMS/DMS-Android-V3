af13a11eeaeb9ebe69e9ae18ec6602a7bc5b0201
Jaewon <turitus@naver.com>
2021-08-28 01:27:42 +0900

    [UPDATE]apply meal ui

--- app/src/main/java/dsm/android/v3/ui/activity/applyMeal/ApplyMealActivity.kt
+++ app/src/main/java/dsm/android/v3/ui/activity/applyMeal/ApplyMealActivity.kt
@@ -5,11 +5,13 @@ import android.os.PersistableBundle
 import dagger.android.support.DaggerAppCompatActivity
 import dsm.android.v3.R
 import kotlinx.android.synthetic.main.activity_apply_meal.*
+import kotlinx.android.synthetic.main.activity_apply_music_dom.*
 
 class ApplyMealActivity:DaggerAppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apply_meal)
+        setSupportActionBar(apply_meal_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
         supportActionBar?.setDisplayShowHomeEnabled(true);
         title = "주말급식 신청"
