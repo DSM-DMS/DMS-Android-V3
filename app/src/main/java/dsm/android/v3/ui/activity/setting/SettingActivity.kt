32c553e011f1dd1b7b49f0b144b3d2bba527fd2a
Jaewon <turitus@naver.com>
2021-09-05 22:58:58 +0900

    [CREATE]setting activity

---
+++ app/src/main/java/dsm/android/v3/ui/activity/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package dsm.android.v3.ui.activity.setting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dsm.android.v3.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
